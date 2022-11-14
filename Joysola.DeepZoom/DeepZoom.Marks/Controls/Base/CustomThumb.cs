f517725ab50966b450c28a7e5b9beec0bfa89fe2
joysola <315718801@qq.com>
2022-11-14 11:13:12 +0800

    1. 更新basemark； 2. 新增部分样式；

---
+++ Joysola.DeepZoom/DeepZoom.Marks/Controls/Base/CustomThumb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls.Primitives;
+
+namespace DeepZoom.Marks.Controls
+{
+    public class CustomThumb : Thumb
+    {
+        public DragDirection DragDirection { get; set; }
+    }
+}
